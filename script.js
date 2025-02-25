2fb862f4a3c87e17b22edc684c726e5a03da80ca
Pr75502 <pp1614582@gmail.com>
2025-02-25 15:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // Create the student object
-const student = {
-  name: "John",
-};
 
-// Function to get all keys from the object
+let student = {
+    name:"johm"
+}
 function getKeys(obj) {
-  return Object.keys(obj);
+    return Object.keys(obj);
 }
 
-// Testing the function
+
 console.log(getKeys(student));
