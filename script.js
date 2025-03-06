02fe34f2b13eaf2677d8e2d850679fb2f115ecf3
Pr75502 <pp1614582@gmail.com>
2025-03-06 19:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Create the student object
 
 const student = {
-	name: "John Doe"
+	name: "John Doe", 
+	age:"24",
+	city:"noida"
 };
 function getKeys(obj) {
 	return Object.keys(obj);
 }
+console.log(getKeys(student));
 
 console.log(getKeys(student));
